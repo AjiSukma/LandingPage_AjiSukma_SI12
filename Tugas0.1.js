1d1335dbe1c447220caace81b7eb0ba81928d3e3
AjiSukma <92713999+AjiSukma@users.noreply.github.com>
2021-12-20 10:22:19 +0700

    Add files via upload
    Aji Sukma_LandingPage_SI12_0110121241

---
+++ Tugas0.1.js
@@ -0,0 +1,20 @@
+function notif() {
+    swal('Maaf','Anda tidak bisa login, coba lagi nanti!','error')
+}
+
+function beli () {
+    swal({
+        title: "YAHHHH, SUDAH HABIS!",
+        text: "Cek lagi besok ya:)",
+        icon: "info",
+        button: "OK",
+      })
+}
+
+function vocer () {
+    swal('Vocher diskon tidak tersedia')
+}
+
+function kirim () {
+    swal('Terima kasih', 'saran dan masukannya kami terima:)','success')
+}
